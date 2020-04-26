972ab9b0330e334cdedd859113feb22df41d4d99
soelmicheletti <48020072+soelmicheletti@users.noreply.github.com>
2020-04-26 19:06:52 +0200

    Create exponentialSearch.java

---
+++ Searching/exponentialSearch.java
@@ -0,0 +1,18 @@
+// AUTHOR: Soel Micheletti
+
+// Requires array to be sorted. Uses binarySearch as subroutine, but can improve runtime in some cases
+public static int exponentialSearch(int[] a, int b) {
+		int r = 1; 
+		while(r<=a.length && a[r]<b){
+			r *= 2; 
+		}
+		
+		int min = Math.min(r, a.length); 
+		int [] bUP = new int[min]; 
+		for(int i = 0; i<bUP.length; i++) {
+			bUP[i] = a[i]; 
+		}
+		 
+		return binarySearch(bUP, b);	
+	}
+  
