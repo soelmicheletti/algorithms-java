ff240f21295a286bdfeda5fc7305190d8f498387
soelmicheletti <48020072+soelmicheletti@users.noreply.github.com>
2020-04-26 20:28:41 +0200

    Create linearSearch.java

---
+++ Searching/linearSearch.java
@@ -0,0 +1,10 @@
+// AUTHOR: Soel Micheletti
+
+// Does not require the array to be sorted
+public static int linearSearch(int[] a, int n) {
+		for(int i = 0; i<a.length;  i++)
+			if(a[i]==n) {
+				return i; 
+			}
+		return -1; 
+	}
