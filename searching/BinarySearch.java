d274381127ad7d8793c9b4dec6cd0530d77eeb97
Soel Micheletti <soel.micheletti@hotmail.it>
2022-06-06 16:56:01 +0200

    Create BinarySearch.java

---
+++ searching/BinarySearch.java
@@ -0,0 +1,21 @@
+// AUTHOR: Soel Micheletti
+
+// Requires array to be sorted
+public static int binarySearch(int[] a, int value) {
+    int left = 0; 
+    int right = a.length; 
+    
+    while(left<=right) {
+        int middle = (left+right)/2; 
+        if(a[middle]==value) {
+            return middle; 
+        }
+        else if(a[middle]<value) {
+            left = middle+1; 
+        }
+        else {
+            right = middle-1; 
+        }
+    }
+    return -1; 
+}
